3584573a334b63904c780e79f11fafb614bcf993
arfoysal <arfoysal.me@gmail.com>
2022-01-07 19:38:54 +0600

    add gitignore and set property

--- src/test/java/testCases/repoDaraz_xpath.java
+++ src/test/java/testCases/repoDaraz_xpath.java
@@ -1,6 +1,9 @@
 package testCases;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 public class repoDaraz_xpath {
+	static Dotenv dotenv = Dotenv.load();
 	public static String selElectAcce = "//span[contains(text(),'Electronic Accessories')]";
 	public static String selCompAcce = "//span[contains(text(),'Computer Accessories')]";
 	public static String selPowCordAdap = "//span[normalize-space()='Power Cord & Adaptors']";
@@ -8,9 +11,9 @@ public class repoDaraz_xpath {
 	public static String selBuyNow = "//span[contains(text(),'Buy Now')]";
 	public static String loginIfram = "//iframe[@class='login-iframe']";
 	public static String selPhoneEmail = "//input[@placeholder='Please enter your Phone Number or Email']";
-	public static String phoneNumber = "your email or phone number";
+	public static String phoneNumber = dotenv.get("phone_number");
 	public static String selPass = "//input[@type='password']";
-	public static String pass = "your password";
+	public static String pass = dotenv.get("password");
 	public static String loginSubmit = "//button[@type='submit']";
 	public static String selPayOp = "//button[contains(text(),'Proceed to Pay')]";
 	public static String selCOD = "//div[@id='automation-payment-method-item-119']";
